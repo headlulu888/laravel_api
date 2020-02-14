3db6639668564c82053facccfdd9e28acc3604ac
headlulu888 <headlulu888@gmail.com>
2020-02-14 13:06:49 +0300

    add product resource

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,30 +3,32 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Products\Product as ProductResource;
 use App\Product;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return Response
+     * @param Request $request
+     * @return JsonResponse|AnonymousResourceCollection
      */
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::paginate();
+        $per_page = $request->per_page ? $request->per_page : 3;
 
-        return response()
-            ->json($products);
+        return ProductResource::collection(Product::paginate($per_page));
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param Request $request
-     * @return Response
+     * @return void
      */
     public function store(Request $request)
     {
@@ -36,23 +38,20 @@ class ProductController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return Response
+     * @param int $id
+     * @return ProductResource
      */
     public function show($id)
     {
-        $product = Product::find($id);
-
-        return response()
-            ->json($product);
+        return new ProductResource(Product::find($id));
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param Request $request
-     * @param  int  $id
-     * @return Response
+     * @param int $id
+     * @return void
      */
     public function update(Request $request, $id)
     {
@@ -62,8 +61,8 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return Response
+     * @param int $id
+     * @return void
      */
     public function destroy($id)
     {
